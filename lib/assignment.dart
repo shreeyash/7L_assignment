832cae7e7ff011f37d2e26591de7e1ba6d1c4aab
Shreeyash Haritashya <haritashya@gmail.com>
2021-08-03 22:22:00 +0530

    chore: code refactored

--- lib/assignment.dart
+++ lib/assignment.dart
@@ -1,182 +1,28 @@
-List<Map<String, dynamic>> inputAttributesJson = [
-  {"name": "mass", "type": "number", "value": 900},
-  {"name": "length", "type": "number", "value": 2},
-  {"name": "width", "type": "number", "value": 1},
-  {"name": "height", "type": "number", "value": 5},
-  {"name": "densityOption", "type": "string", "value": "specGravity"}
-];
-
-List<Map<String, dynamic>> computedAttributesJson = [
-  {"name": "volume", "expression": "length * width * height"},
-  {
-    "name": "density",
-    "expression":
-        "IIF(densityOption == 'specGravity', mass/(length * width * height * 0.9), mass / (length * width * height))"
-  }
-];
-
-Map<String, dynamic> variableTypes = {};
-Map<String, dynamic> variableValues = {};
+import 'package:sevenlake_assignment/compute.dart';
 
 void main() {
-  inputAttributesJson.forEach(
-    (element) {
-      variableValues.addAll(
-        {element['name']: element['value']},
-      );
-    },
-  );
+  Compute _compute = Compute();
 
-  print(variableValues);
+// Input Json
+  List<Map<String, dynamic>> inputJsonAttribute = [
+    {'name': 'mass', 'type': 'number', 'value': 900},
+    {'name': 'length', 'type': 'number', 'value': 2},
+    {'name': 'width', 'type': 'number', 'value': 1},
+    {'name': 'height', 'type': 'number', 'value': 5},
+    {'name': 'densityOption', 'type': 'string', 'value': 'specGravity'}
+  ];
 
-  inputAttributesJson.forEach(
-    (element) {
-      variableTypes.addAll(
-        {element['name']: element['type']},
-      );
-    },
-  );
-
-  print(variableTypes);
-
-  bool expressionState = evaluateIf(
-      "IIF(densityOption == 'specGravity', mass/(length * width * height * 0.9), mass / (length * width * height))");
-  print("Expression State - $expressionState");
-}
-
-// This functions evaluates the IIF condition
-bool evaluateIf(String exp) {
-  bool isConditionTrue;
-  double exp1;
-  double exp2;
-  String trimmedExpression = exp.trim();
-  var startIndex = trimmedExpression.indexOf('(');
-  var lastIndex = trimmedExpression.lastIndexOf(')');
-  trimmedExpression =
-      trimmedExpression.substring(startIndex + 1, lastIndex).trim();
-  var expList = trimmedExpression.split(",");
-  isConditionTrue = evaluateLogicExpression(expList[0]);
-  evaluateMathExpression(expList[1].trim().split(' '));
-  print("Expression 1 - ${expList[1].trim().split(' ')}");
-  print("Expression 2 - ${expList[2].trim().split(' ')}");
-  return isConditionTrue;
-//         if (isConditionTrue) {
-//             return exp1;
-//         } else {
-//             return exp2;
-//         }
-}
-
-dynamic evaluateMathExpression(List<String> expression) {
-  var result = 0.00;
-  String operator;
-  print("Expression - $expression");
-
-  
-
-
-  if (expression.contains('(')) {
-    print('Expression contains (');
-    result = evaluateBracketExpression(expression);
-  } else {
-    print("Inside else");
-    for (var i = 0; i < expression.length; i++) {
-      print("Expression of i - ${variableValues[expression[i]]}");
-      var expressionValue =
-          variableValues[expression[i]] == null && expression[i].length > 1
-              ? expression[i]
-              : variableValues[expression[i]] ?? 1.0;
-
-      print("Expression value - $expressionValue");
-
-      switch (expression[i]) {
-        case '+':
-          operator = '+';
-          break;
-        case '-':
-          operator = '-';
-          break;
-        case '*':
-          operator = '*';
-          break;
-        case '/':
-          operator = '/';
-          break;
-        default:
-          print("Default case");
-          if (operator == null) {
-            result = double.parse(expressionValue) * 1.0;
-          } else {
-            print("else default case");
-          }
-      }
+// Values to be calculated from below given Json
+  List<Map<String, dynamic>> computeJsonAttribute = [
+    {'name': 'volume', 'expression': 'length * width * height'},
+    {
+      'name': 'density',
+      'expression':
+          "IIF(densityOption == 'specGravity', mass / ( length * width * height * 0.9 ), mass / ( length * width * height ) )"
     }
-  }
-  return result;
-}
+  ];
+  List<Map<String, dynamic>> result =
+      _compute.computeValues(inputJsonAttribute, computeJsonAttribute);
 
-evaluateBracketExpression(List<String> expression) {
-  print("Inside evaluateBracketExpression");
-  int startIndex;
-  int endIndex;
-  double evaluateSubExpression;
-  for (var i = 0; i < expression.length; i++) {
-    if (expression[i] == '(') {
-      startIndex = i;
-    } else if (expression[i] == ')') {
-      endIndex = i;
-      break;
-    }
-  }
-  // evaluating the contents of the bracket.
-  print("evaluating the contents of the bracket");
-  evaluateSubExpression = evaluateMathExpression(
-      expression.getRange(startIndex + 1, endIndex).toList());
-  print(
-      'From Evaluate Bracket Exp: evaluateSubExpression or result : $evaluateSubExpression');
-  // replacing brackets in array by evaluateSubExpression value
-  expression.replaceRange(
-      startIndex, endIndex + 1, [evaluateSubExpression.toString()]);
-
-  print('From Evaluate Bracket Exp: evaluateSubExpression :  $expression');
-  // evaluating the new array again, by calling it recrusively
-  return evaluateMathExpression(expression);
-}
-
-/// This functions evaluates logical expression as true or false
-bool evaluateLogicExpression(String expression) {
-  var rightExp;
-  List<String> operands;
-  bool result;
-  operands = expression.split(' ');
-  operands[2] = operands[2].substring(1, operands[2].length - 1).trim();
-  if (variableTypes[operands[0]] == "number") {
-    rightExp = double.parse(operands[2]);
-  } else {
-    rightExp = operands[2];
-  }
-  switch (operands[1]) {
-    case "==":
-      result = variableValues[operands[0]] == rightExp;
-      break;
-    case "<":
-      result = variableValues[operands[0]] < rightExp;
-      break;
-    case ">":
-      result = variableValues[operands[0]] > rightExp;
-      break;
-    case "!=":
-      result = variableValues[operands[0]] != rightExp;
-      break;
-    case "<=":
-      result = variableValues[operands[0]] <= rightExp;
-      break;
-    case ">=":
-      result = variableValues[operands[0]] >= rightExp;
-      break;
-    default:
-      result = true;
-  }
-  print("Log Exp result: {} - $result");
-  return result;
+  print("Output -  $result");
 }
---
+++ lib/compute.dart
@@ -0,0 +1,196 @@
+class Compute {
+  Map<String, dynamic> variableValues;
+  Map<String, dynamic> variableTypes;
+
+// evaluateBracketExpression is a recrusive function and it evaluates the Bracket expression, takes List of String expressions as an argument,
+// eg: [mass, /, (, length, *, width, *, height, *, 0.9, )]
+// and evaluate if the list has bracket ( , ) or not and accordingly at the start and end index
+// to calculate values for the particular expression which lies inside bracket by calling evaluateMathExpression as and when needed.
+  double evaluateBracketExpression(List<String> expressionValue) {
+    print('From Bracket Math Exp: Input = $expressionValue');
+    int startIndex = 0;
+    int endIndex = 10000;
+    double subExpressionValue;
+    for (var i = 0; i < expressionValue.length; i++) {
+      if (expressionValue[i] == '(') {
+        startIndex = i;
+      } else if (expressionValue[i] == ')') {
+        endIndex = i;
+        break;
+      }
+    }
+    subExpressionValue = evaluateMathExpression(
+        expressionValue.getRange(startIndex + 1, endIndex).toList());
+    print(
+        'From Evaluate Bracket Exp: subExpValue or result = $subExpressionValue');
+    expressionValue.replaceRange(
+        startIndex, endIndex + 1, [subExpressionValue.toString()]);
+    print('From Evaluate Bracket Exp: subExpList =  $expressionValue');
+    return evaluateMathExpression(expressionValue);
+  }
+
+// evaluateMathExpression function evaluates the Mathematical expression, takes List of String expressions as an argument,
+// eg. mass, /, (, length, *, width, *, height, *, 0.9, )
+// and evaluate all the possible mathematical operator conditions(+ , - , * , / ),
+// and calculates the value based on the given operator and returns the calculated value of it.
+  double evaluateMathExpression(List<String> expressionValue) {
+    print('From Evaluate Math Exp: Input = $expressionValue');
+    String operator;
+    var result = 0.0;
+    if (expressionValue.contains('(')) {
+      result = evaluateBracketExpression(expressionValue);
+    } else {
+      for (var i = 0; i < expressionValue.length; i++) {
+        var value = variableValues[expressionValue[i]] == null &&
+                expressionValue[i].length > 1
+            ? double.parse(expressionValue[i])
+            : variableValues[expressionValue[i]] ??
+                double.tryParse(expressionValue[i]);
+        switch (expressionValue[i]) {
+          case '+':
+            operator = '+';
+            break;
+          case '-':
+            operator = '-';
+            break;
+          case '*':
+            operator = '*';
+            break;
+          case '/':
+            operator = '/';
+            break;
+          default:
+            if (operator == null) {
+              result = value * 1.0;
+            } else {
+              switch (operator) {
+                case '+':
+                  result = result + value;
+                  operator = null;
+                  break;
+                case '-':
+                  result = result - value;
+                  operator = null;
+                  break;
+                case '*':
+                  result = result * value;
+                  operator = null;
+                  break;
+                case '/':
+                  result = result / value;
+                  operator = null;
+                  break;
+                default:
+                  result = result;
+                  operator = null;
+              }
+            }
+        }
+      }
+    }
+    return result;
+  }
+
+// evaluateLogicExpression function evaluates the Logical expression, takes String expression as an argument,
+// and evaluate all the possible logical conditions(== , > , < , >= , <=, !=),
+// which returns boolean value based on the different conditions
+  bool evaluateLogicExpression(String expression) {
+    var value;
+    List<String> operands;
+    bool result;
+    operands = expression.split(' ');
+    print('From evaluateLogExp $operands');
+    operands[2] = operands[2].substring(1, operands[2].length - 1).trim();
+    if (variableTypes[operands[0]] == 'number') {
+      value = double.parse(operands[2]);
+    } else {
+      value = operands[2];
+    }
+    switch (operands[1]) {
+      case '==':
+        result = variableValues[operands[0]] == value;
+        break;
+      case '>':
+        result = variableValues[operands[0]] > value;
+        break;
+      case '<':
+        result = variableValues[operands[0]] < value;
+        break;
+      case '>=':
+        result = variableValues[operands[0]] >= value;
+        break;
+      case '<=':
+        result = variableValues[operands[0]] <= value;
+        break;
+      case '!=':
+        result = variableValues[operands[0]] != value;
+        break;
+      default:
+        result = true;
+    }
+    print('From Evaluate Log Exp result = $result');
+    return result;
+  }
+
+// evaluateIf function evaluates the If expression, takes String expression as an argument,
+// and evaluate the possible logical conditions(function - evaluateLogicExpression),
+// which returns boolean value, if it's true, calcualtes the math expression of 1st argument else
+// calcualtes the math expression of 2nd argument
+  double evaluateIf(String expression) {
+    bool isConditionTrue;
+    String trimmedExpression = expression.trim();
+    var startIndex = trimmedExpression.indexOf('(');
+    var lastIndex = trimmedExpression.lastIndexOf(')');
+    trimmedExpression =
+        trimmedExpression.substring(startIndex + 1, lastIndex).trim();
+    var expressionList = trimmedExpression.split(",");
+    isConditionTrue = evaluateLogicExpression(expressionList[0]);
+    evaluateMathExpression(expressionList[1].trim().split(' '));
+    if (isConditionTrue) {
+      return evaluateMathExpression(expressionList[1].trim().split(' '));
+    } else {
+      return evaluateMathExpression(expressionList[2].trim().split(' '));
+    }
+  }
+
+// evaluateExp function will evaluate the expression, takes String expression as an argument,
+// if it's IIF Expression, it calls evaluateIf otherwise evaluateMathExpression
+// and returns the result returned from the functions being called.
+  double evaluateExp(String expression) {
+    double result;
+    if (expression.contains('IIF')) {
+      result = evaluateIf(expression);
+    } else {
+      result = evaluateMathExpression(expression.trim().split(' '));
+    }
+    return result;
+  }
+
+  List<Map<String, dynamic>> computeValues(
+      List<Map<String, dynamic>> inputJsonAttribute,
+      List<Map<String, dynamic>> computeJsonAttribute) {
+    List<Map<String, dynamic>> resultMap;
+    resultMap = [];
+    variableValues = {};
+    inputJsonAttribute.forEach((element) {
+      variableValues?.addAll(
+        {element['name']: element['value']},
+      );
+    });
+    variableTypes = {};
+    inputJsonAttribute.forEach((element) {
+      variableTypes?.addAll(
+        {element['name']: element['type']},
+      );
+    });
+    computeJsonAttribute.forEach((Map<String, dynamic> value) {
+      Map<String, dynamic> resultValue;
+      resultValue = {
+        'name': value['name'],
+        'value': evaluateExp(value['expression']),
+      };
+      resultMap.add(resultValue);
+    });
+    return resultMap;
+  }
+}
